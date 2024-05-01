e1705df220f261d09da09520efe0044035236617
RotesBlatt <blauerhut04@gmail.com>
2024-05-01 23:12:09 +0200

    Added search string input support. Searching for a video is now possible without having to use a Youtube link

--- src/bot_commands/play.ts
+++ src/bot_commands/play.ts
@@ -13,19 +13,29 @@ export default {
         .setName('play')
         .setDescription('Play a song from Youtube')
         .addStringOption(option => option
-            .setName('song')
-            .setDescription('Song URL from Youtube')
-            .setRequired(true)
+            .setName('url')
+            .setDescription('Youtube video URL')
+        )
+        .addStringOption(option => option
+            .setName('search')
+            .setDescription('Youtube search string')
         )
         .setDMPermission(false),
 
     async execute(interaction: ChatInputCommandInteraction) {
         await interaction.deferReply();
-        const songURL = interaction.options.getString('song', true);
 
         const client = interaction.client as ClientAdapter;
         const iconURL = client.user?.avatarURL()!;
-        
+
+        const songURL = interaction.options.getString('url', false);
+        const searchString = interaction.options.getString('search', false);
+        if (!songURL && !searchString) {
+            logger.warn('No parameters given in order to play a song');
+            interaction.editReply({ embeds: [embed.errorOccurred('No URL or search input was provided', iconURL)] })
+            return;
+        }
+
         const guildId = interaction.guild?.id!;
         const guildManager = client.guildManagerCollection.get(guildId);
         if (!guildManager) {
@@ -41,11 +51,30 @@ export default {
             adapterCreator: interaction.guild?.voiceAdapterCreator!,
         });
 
-        const songInfo = await playdl.video_info(songURL);
-        guildManager.audioPlayer.addSongToQueue(songInfo);
+        var songInfo = null;
+        if (songURL) {
+            songInfo = await playdl.video_info(songURL);
+            guildManager.audioPlayer.addSongToQueue(songInfo);
+        } else if (searchString) {
+            const searchResults = await playdl.search(searchString, {
+                limit: 1,
+            });
 
+            const searchResultUrl = searchResults.at(0)?.url;
+            //Check that a video was found for the given search string, otherwise there is no song to be played
+            if (!searchResultUrl) {
+                logger.warn(`Search for '${searchString}' failed, found no matching video on Youtube`);
+                interaction.editReply({ embeds: [embed.errorOccurred(`Searching for \`${searchString}\` resulted in nothing, try something else`, iconURL)] });
+                return;
+            }
+
+            songInfo = await playdl.video_info(searchResultUrl);
+            guildManager.audioPlayer.addSongToQueue(songInfo);
+        }
+
+        // Add to queue if there the player is playing a song, otherwise play the song which was just added
         if (guildManager.audioPlayer.isPlaying()) {
-            await interaction.editReply({ embeds: [embed.addQueue(guildManager, iconURL)] });
+            await interaction.editReply({ embeds: [embed.addQueue(songInfo!, iconURL)] });
         } else {
             await guildManager.audioPlayer.startPlaying();
             await interaction.editReply({ embeds: [embed.currentSong(guildManager, iconURL)] });
